e3b1ebe69dd5707daf74ac8d3568c88ffc5e14ac
hi9425 <hi9425@hanmail.net>
2016-04-05 00:30:10 +0900

    2016_04_05 과제제출

---
+++ w5main(2014_04_05).py
@@ -0,0 +1,35 @@
+﻿import turtle
+
+wn=turtle.Screen()
+
+t1=turtle.Turtle()
+
+
+def BMI():
+    weight=raw_input("input your weight : ")
+    height=raw_input("input your Height : ")
+    weight=float(weight)
+    height=float(height)
+    bheight=height/100
+    bmi=weight/(bheight*bheight)
+    if(bmi<18.5):
+        print "low weight"
+    elif(18.5<=bmi<25):
+        print"nomal"
+    elif(25<=bmi<30):
+        print "middle obesity"
+    elif(30<=bmi):
+        print "high obesity"
+    else:
+        print "Error"
+
+    t1.write("승훈 과제 끝")
+
+
+BMI()
+def main():
+	BMI()
+
+
+wn.exitonclick()
+
