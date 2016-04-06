0e2602afe27388c046bd09653f6584b94ab678b9
hi9425 <hi9425@hanmail.net>
2016-04-06 15:50:01 +0900

    윤년

---
+++ w7main_04_06__(2).py
@@ -0,0 +1,39 @@
+﻿"""
+@author 이승훈
+@since 04_06
+"""
+def hun():
+
+    year=input("input the year")
+
+
+    if(year%4==0) and (year%100!=0 or year%400==0):
+        print"leap year"
+    else:
+        print"not leap year"
+hun()
+import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+t1.write("윤년")
+wn.exitonclick()
+
+"""
+repeat
+
+start
+:input year;
+if(year%4==0)and(year%100!=0 or year%400==0)
+:print leap year;
+else(fales)
+:print not leap lear;
+endif
+stop
+
+
+ 
+
+"""
+
+def main():
+     hun()
