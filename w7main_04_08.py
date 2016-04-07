c63b0a1c60db2bffe50704f263bbed7f21e04a99
hi9425 <hi9425@hanmail.net>
2016-04-08 00:27:45 +0900

    up n down

---
+++ w7main_04_08.py
@@ -0,0 +1,61 @@
+﻿"""
+@author LeeSeungHUn
+@since 201415058
+"""
+
+
+def UpDown():
+	import random
+	ai = input("choice  range  ")
+	r1 = random.randrange(1,ai+1)
+	global pl
+	print "Start!"
+
+
+	def game():
+	    pl = input("choice number")
+	    if (r1>pl):
+	        result = "Up"
+	        print result
+	        game()
+	    elif (r1<pl):
+	        result = "Down"
+	        print result
+	        game()
+	    else :
+	        result = "Correct"
+	        print result
+        	if (result=="Correct"):
+	            wn = raw_input("congratulation ")
+	game()
+
+"""
+
+@startuml
+title Up n Down Game
+start
+:Player : 범위선택;
+:Computer AI : 범위 안의 숫자 선택;
+:시작;
+:숫자 입력; 
+while(플레이)
+if (숫자 비교) then (Plater N < AI N)
+:Up;
+else (Player N > AI B)
+:Down;
+endif
+endwhile
+:Player N = AI N;
+:"축하";
+:끝;
+stop
+@enduml
+
+"""
+
+	
+def main():
+    UpDown()
+
+if __name__=="__main__":
+    main()
