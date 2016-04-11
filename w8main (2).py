cf5c03863d012673fd67370747b2528d9dae32cc
hi9425 <hi9425@hanmail.net>
2016-04-11 14:31:06 +0900

    w8main

---
+++ w8main (2).py
@@ -0,0 +1,12 @@
+﻿x1=list()
+def sumlist(alist):
+	sum=0
+	for i in range(0,1000):
+		if i%4==0 and i%5!=0:
+			alist.append(i)
+	for i in range(0,len(alist)):
+		sum=sum+alist[i]
+	return sum
+print sumlist(x1)
+
+raw_input("finish")
