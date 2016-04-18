41bede24cf1a8f137b7e74962ac291000f66404d
hi9425 <hi9425@hanmail.net>
2016-04-18 14:28:17 +0900

    16.04.18

---
+++ w9main.py
@@ -0,0 +1,20 @@
+﻿import turtle
+
+wn=turtle.Screen()
+
+t1=turtle.Turtle()
+
+tracks=list()
+
+def lab7():
+    for i in range(0,4):
+       tracks.append(t1.pos()) 
+       t1.fd(50)
+       t1.right(90)
+       print tracks
+
+def main():
+	lab7()
+if __name__=="__main__":
+	main()	
+wn.exitonclick()
