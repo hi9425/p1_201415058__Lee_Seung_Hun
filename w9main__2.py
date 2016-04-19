c3f5becffee1d643a8e0e0495299f1e9ad383cb1
hi9425 <hi9425@hanmail.net>
2016-04-19 13:21:44 +0900

    이승훈

---
+++ w9main__2.py
@@ -0,0 +1,25 @@
+﻿import turtle
+
+wn=turtle.Screen()
+
+t1=turtle.Turtle()
+
+t1.shape("turtle")
+
+c=dict()
+
+c=({0:(0,0),1:(200,0),2:(200,200),3:(0,200),4:(0,0)})
+
+def lab7():
+    for i in range(1,5):
+        t1.goto(c[i])
+    for k in range(1,5):
+        print c[k]
+
+def main():
+    lab7()
+
+if __name__=="__main__":
+    main()
+
+wn.exitonclick()
