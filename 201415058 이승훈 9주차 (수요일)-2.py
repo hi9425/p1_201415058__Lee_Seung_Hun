5b34f7156e5733a3018fd221c15c7aab81ce0123
Lee Seung Hun <hi9425@hanmail.net>
2016-05-06 05:26:52 +0900

    Added files via upload

---
+++ 201415058 이승훈 9주차 (수요일)-2.py
@@ -0,0 +1,25 @@
+﻿import math
+l=tuple()
+l=[(74425,76326),(61164,61636),(109688,115744),(144796,146776),(174996,181676),(177841,177434),(204630,205980),(223373,232245),(161055,166130),(171576,176735),(279169,293772),(239450,251066),(148690,156510),(182977,196992),(237792,242641),(283869,296762),(209344,210282),(118965,114441),(186503,186856),(195734,203014),(254381,249472),(212401,229111),(271654,295354),(319197,335045),(229829,231671)]
+len(l)
+sum=0
+sum1=0
+sum2=0
+people=list()
+for q in l:
+    sum=sum+q[0]
+    sum1=sum1+q[1]
+    sum2=q[0]+q[1]
+    people.append(sum2)
+print sum
+male= sum/len(l)
+print "average seoul is %s"%male
+print sum1
+female=sum1/len(l)
+print "average seoul is %s"%female
+import matplotlib
+import matplotlib.pyplot as plt
+print people
+lsh=dict()
+plt.bar(range(len(people)),people,align='center')
+plt.show()
---
+++ 201415058 이승훈 9주차 (수요일).py
@@ -0,0 +1,16 @@
+﻿Locations=tuple()
+Locations=((37.571616,126.976584),(37.565596,126.966542),(37.576385,126.985676),(37.576385,126.985676))
+import math
+distanceex=math.sqrt((1-3)**2+ (2-4)**2)
+print Locations[0][0]
+for i in Locations:
+        print "x={0},y={1}".format(i[0],i[1]) 
+name='LSH'
+where='home'
+gbloc=(37.575723,126.973526)
+dist=list()
+for t in Locations:
+        distance=math.sqrt((gbloc[0]-t[0])**2+(gbloc[1]-t[1])**2)
+        dist.append(distance)
+print "the minimum distance is %s" %min(dist)
+input()
